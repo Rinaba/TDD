ff051c55b08171acfec096f8feb64055e229dd7d
RyoichiInaba <religio6329@gmail.com>
2022-02-27 20:57:00 +0900

    テストをリファクタリング。1を渡すと文字列'1'を返す仮実装

--- index.test.js
+++ index.test.js
@@ -1,9 +1,5 @@
 const fizzBuzz = require('./index');
 
 test("1を渡すと文字列'1'を返す", () => {
-    // 準備 (Arrange)
-    // 実行 (Act)
-    const actual = fizzBuzz(1);
-    // 検証 (Assertion)
-    expect(actual).toBe('1');
+    expect(fizzBuzz(1)).toBe('1');
 });

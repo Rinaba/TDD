2e4b0bc7b14b323faa3c94817b4b1ab095978406
RyoichiInaba <religio6329@gmail.com>
2022-03-06 21:16:07 +0900

    3を渡すと文字列'Fizz'を返すテストを作成

--- index.test.js
+++ index.test.js
@@ -7,3 +7,7 @@ test("1を渡すと文字列'1'を返す", () => {
 test("2を渡すと文字列'2'を返す", () => {
     expect(fizzBuzz(2)).toBe('2');
 });
+
+test("3を渡すと文字列'Fizz'を返す", () => {
+    expect(fizzBuzz(3)).toBe('Fizz');
+});
